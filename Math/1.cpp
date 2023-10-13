7561b87de643b689f5d625062fcd3180c452e2cc
ABHISHEK KUMAR <abhishek.nitdelhi@gmail.com>
2023-10-13 20:22:33 +0530

    Math first questio

---
+++ Math/1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+// Simple implementation is done just take the range b c c will be the traingle
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+void solve(){
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<b<<" "<<c<<" "<<c<<endl;
+    return;
+}
+
+int main()
+{
+    fastio;
+    int n;
+    cin>>n;
+    while(n--) solve();
+    return 0;
+}
